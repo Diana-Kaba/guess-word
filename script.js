7f43e4e14cacc15e6d6f963b1e75b09abecbaa2c
Diana-Kaba <kabadiana327@gmail.com>
2022-12-10 14:11:22 +0200

    Third commit

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let fruits = ["apple", "pear", "orange", "grape"];
+let vegetables = ["cucumber", "tomato", "salad greens", "onion"];
+let choise = prompt("What do you want, vegetables or fruits?");
+
 window.onload = function () {
   let images = document.getElementsByTagName("img");
   for (let i = 0; i < images.length; i++) {
@@ -5,20 +9,39 @@ window.onload = function () {
   }
 };
 
-let words = ["apple", "pear"];
+function check(event) {
+  let image = event.target;
+  let name = image.id;
+  let p = prompt("What's this?");
+  if (p == name) {
+    alert("Correctly!");
+  } else {
+    alert("Wrong!");
+  }
+}
 
-function check(arr) {
-  // let image = event.target;
-  // let name = image.id;
-  // let p = prompt("Что за фрукт?");
-  // if (p == name) {
-  //   alert("Правильно!");
-  // } else {
-  //   alert("Не правильно!");
-  // }
+function random(arr) {
+  let randomWords = [];
+  let n;
+  for (let i = 0; i < 3; i++) {
+    n = Math.floor(Math.random() * arr.length);
+    randomWords.push(arr[n]);
+  }
+  return randomWords;
+}
+
+function gen(arr) {
   for (let i = 0; i < arr.length; i++) {
     document.write('<img id="' + arr[i] + '" src="images/' + arr[i] + '.png">');
   }
 }
 
-check(words);
+function call() {
+  if (choise == "vegetables") {
+    gen(random(vegetables));
+  } else {
+    gen(random(fruits));
+  }
+}
+
+call();
